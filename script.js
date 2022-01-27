a10fc56d43cb9d0ebb3a8213c3d1c493ceb38f11
SQuinones <78192383+SQuinones@users.noreply.github.com>
2022-01-27 13:20:02 -0500

    adding 3d boxes background project

---
+++ script.js
@@ -0,0 +1,20 @@
+//declaring variables
+const boxesContainer = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+
+//adding evenlistener when the user click to the bottom
+btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+function createBoxes() {
+    
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            const box = document.createElement('div')
+            box.classList.add('box')
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+            boxesContainer.appendChild(box)
+        }
+    }
+}
+
+createBoxes()
